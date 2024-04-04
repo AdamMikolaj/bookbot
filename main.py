169f22d886598461d55fdbff2b1d3c4cf6072f92
AdamMikolaj <adammikolaj@gmail.com>
2024-04-04 16:12:42 -0600

    fixed a bug

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import os
+
 def main():
-    book_path = "github.com/adammikolaj/bookbot/books/frankenstein.txt"
+    base_path = "github.com/adammikolaj/bookbot/books/"
+    book_filename = "frankenstein.txt"
+    book_path = os.path.join(os.getcwd(), base_path, book_filename)
     text = get_book_text(book_path)
     words = len(text.split())
     print(words)
 
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
 
 main()
