3fd25f28aef848da15a8a980e63cee1983c6882e
AdamMikolaj <adammikolaj@gmail.com>
2024-04-10 11:18:30 -0600

    Added the word count function

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ def main():
     book_path = os.path.join(os.getcwd(), base_path, book_filename)
     text = get_book_text(book_path)
     words = len(text.split())
+    l_words = str(text).lower()   
+    word_count(l_words)
     print(words)
+    
 
 def get_book_text(path):
+    """
+    Read the content of the file located at the specified path and return it as a string.
+    """
     with open(path) as f:
         return f.read()
+
+def word_count(l_words):
+    wc = {}
+    for w in l_words:  
+        if w in wc:
+            wc[w] += 1
+        else:
+            wc[w] = 1
+    print(wc)
+
 
 main()
