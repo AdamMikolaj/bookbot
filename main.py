e8a5ba8de10709ba68eb36796cf636125a9a34de
AdamMikolaj <adammikolaj@gmail.com>
2024-04-23 01:03:34 -0600

    no idea what I did

--- main.py
+++ main.py
@@ -6,26 +6,57 @@ def main():
     book_path = os.path.join(os.getcwd(), base_path, book_filename)
     text = get_book_text(book_path)
     words = len(text.split())
-    l_words = str(text).lower()   
-    word_count(l_words)
-    print(words)
+    l_words = str(text).lower()  
+    num_words = get_num_words(text) 
+   # word_count(l_words)
     
+    #this is the part I added
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+    #end of the part I added
+    
+  
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
-    """
-    Read the content of the file located at the specified path and return it as a string.
-    """
     with open(path) as f:
         return f.read()
-
-def word_count(l_words):
-    wc = {}
-    for w in l_words:  
-        if w in wc:
-            wc[w] += 1
-        else:
-            wc[w] = 1
-    print(wc)
-
 
 main()
